4f7665382d4a5918a369e335c47beb21d23cbe70
JulianGerrit <37937880+JulianGerrit@users.noreply.github.com>
2019-09-06 17:20:46 +0200

    Website template
    Template met React als front-end en Node/Express als back-end.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+﻿import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+import Index from "./components/Index";
+//TODO Web Template Studio: Add routes for your new pages here.
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Redirect exact path = "/" to = "/Index" />
+          <Route path = "/Index" component = { Index } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
