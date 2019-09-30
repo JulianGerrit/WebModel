e7e2118f87533da62333aab82f3abf65e65c4559
Frederick <steenooo5@gmail.com>
2019-09-30 23:27:06 +0200

    fixed.

--- src/components/Index/graph.jsx
+++ src/components/Index/graph.jsx
@@ -1,59 +1,64 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import styles from "./index.module.css";
 import {ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
-  
+
 
 export default class Graph extends Component {
 
     render() {
-        let xAxisVariableInData;
-        let xAxisVariableLocation;
-        for(let i = 0; i < this.props.res.vars.length; i++){
-            if(this.props.res.vars[i].name === this.props.xAxis){
-                xAxisVariableInData = true;
-                xAxisVariableLocation = i;
+
+        let vars = this.props.res.vars;
+
+        let result =[];
+        let xAxis;
+
+        //TODO Implement this.props.xAxis. Didn't work because it's not initialized.
+
+        for(let a of vars) {
+            if(a.name === "x") {
+                xAxis = a;
+                break;
             }
         }
-        if(!xAxisVariableInData) {
+        if(xAxis == null) {
             return (
                 <React.Fragment>
                     <p>Defined x-axis variable {this.props.xAxis} is not in data.</p>
-                {this.props.res.vars ? JSON.stringify(this.props.res.vars) : ""}
-                </React.Fragment>
-            );
-        }else{
-            let components = [];
-            for(let line of this.props.res.vars){
-                if(this.props.xAxis !== line.name){
-                    let lineData = [];
-                    for (let i = 0; i < line.data.length; i++) {
-                        lineData.push({x: this.props.res.vars[xAxisVariableLocation][i], y: line.data[i]});
-                    } 
-                    components.push(<Scatter name={line.name} data={lineData} fill={line.color.hex} line />);
-                }
-                
+                    {this.props.res.vars ? JSON.stringify(this.props.res.vars) : ""}
+                </React.Fragment>);
+        }
+
+        for(let line of vars) {
+            let xdata = [];
+            if(line === xAxis) continue;
+
+            for (let i = 0; i < line.data.length; i++) {
+                let dx = xAxis.data[i];
+                let dy = line.data[i];
+                console.log(dx + ",  " + dy +  "")
+                xdata.push({x: xAxis.data[i], y: line.data[i]})
             }
+            result.push(<Scatter name={line.name} data={xdata} fill={line.color.hex} line/>)
+        }
             return (
                 <React.Fragment>
-                        <ScatterChart
-                            width={500}
-                            height={400}
-                            margin={{
+                    <ScatterChart
+                        width={500}
+                        height={400}
+                        margin={{
                             top: 20, right: 20, bottom: 20, left: 20,
-                            }}
-                        >
-                            <CartesianGrid />
-                            <XAxis type="number" dataKey="x" name="stature"  />
-                            <YAxis type="number" dataKey="y" name="weight" />
-                            {components}
-                        </ScatterChart>
+                        }}
+                    >
+                        <CartesianGrid/>
+                        <XAxis type="number" dataKey="x" name="stature"/>
+                        <YAxis type="number" dataKey="y" name="weight"/>
+                        {result}
+                    </ScatterChart>
 
-                        {this.props.res.vars ? JSON.stringify(this.props.res.vars) : ""}
-                        <button className={styles.submitButton}>Export CSV</button>
-                        
+                    {this.props.res.vars ? JSON.stringify(this.props.res.vars) : ""}
+                    <button className={styles.submitButton}>Export CSV</button>
+
                 </React.Fragment>
-            )
+            );
         }
-        
-    }
 }
