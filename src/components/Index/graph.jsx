fec35290a667ebd1e6f0ab2df9a9fee577fdcb95
Zipdox <alex.wielinga@gmail.com>
2019-11-18 17:42:57 +0100

    fix graphing stuff

--- src/components/Index/graph.jsx
+++ src/components/Index/graph.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import styles from "./index.module.css";
-import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import {ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Label} from 'recharts';
+// import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 import Download from '@axetroy/react-download';
 
 
@@ -31,7 +32,7 @@ export default class Graph extends Component {
                 linedata.push({x: xvar.data[i], y: yvar.data[i]});
                 csvdata[i+1].push(yvar.data[i]);
             }
-            result.push(<Scatter name={yvar.name} data={linedata} fill={yvar.color.hex} line/>);
+            result.push(<Scatter name={yvar.name} dataKey={yvar.name} data={linedata} fill={yvar.color.hex} line/>);
         }
 
         var csvstring = "";
@@ -44,6 +45,54 @@ export default class Graph extends Component {
                 csvstring += `${csvdata[i].join(',')}\n`;
             }
         }
+
+        
+
+
+
+
+
+
+        // let vars = this.props.res.vars;
+        // console.log(vars);
+        
+        // var lines = [];
+        // var csvdata = [];
+        // var xvar;
+        // var yvars = [];
+        // csvdata.push([])
+        // for(var variable of vars) {
+        //     csvdata[0].push(`"${variable.name}"`);
+        //     if(variable.axis === true) xvar = variable;
+        //     if(variable.axis === false) yvars.push(variable);
+        // }
+
+        // var chartdata = [];
+        // for(let i = 0; i < xvar.data.length; i++){
+        //     csvdata.push([xvar.data[i]]);
+        //     chartdata.push({[xvar.name]: xvar.data[i]});
+        // }
+
+        // let result = [];
+        // for(var yvar of yvars){
+        //     for(let i = 0; i < yvar.data.length; i++){
+        //         csvdata[i+1].push(yvar.data[i]);
+        //         chartdata[i][yvar.name] = yvar.data[i];
+        //     }
+        //     lines.push(<Line type="basis" dataKey={yvar.name} stroke={yvar.color.hex} />);
+        // }
+        // console.log(chartdata);
+
+        // var csvstring = "";
+        // for(let i = 0; i < csvdata.length; i++){
+        //     if(i == 0){
+        //         csvstring += `${csvdata[i].join(',')}\n`;
+        //     }else if(i == (csvdata.length - 1)){
+        //         csvstring += csvdata[i].join(',');
+        //     }else{
+        //         csvstring += `${csvdata[i].join(',')}\n`;
+        //     }
+        // }
 
 
             return (
@@ -58,7 +107,9 @@ export default class Graph extends Component {
                         <CartesianGrid/>
                         <Tooltip/>
                         <Legend />
-                        <XAxis type="number" dataKey="x" name={xvar.name}/>
+                        <XAxis type="number" dataKey="x" name={xvar.name}>
+                            <Label value={xvar.name} offset={0} position="insideBottom" />
+                        </XAxis>
                         <YAxis type="number" dataKey="y"/>
                         {result}
                     </ScatterChart>
@@ -75,5 +126,37 @@ export default class Graph extends Component {
 
                 </React.Fragment>
             );
+
+            // return (
+            //     <React.Fragment>
+            //         <LineChart
+            //             width={500}
+            //             height={400}
+            //             data={chartdata}
+            //             margin={{
+            //                 top: 20, right: 20, bottom: 20, left: 20,
+            //             }}
+            //         >
+            //             <CartesianGrid/>
+            //             <Tooltip/>
+            //             <Legend/>
+            //             <XAxis dataKey={xvar.name}/>
+            //             <YAxis/>
+            //             {lines}
+            //         </LineChart>
+                    
+            //         {/* <textarea>
+            //             {this.props.res.vars ? JSON.stringify(this.props.res.vars) : ""}
+            //         </textarea> */}
+                    
+            //         <Download
+            //             file="webmodel-data.csv"
+            //             content={csvstring}>
+            //             <button className={styles.submitButton}>Download Data</button>
+            //         </Download>
+
+            //     </React.Fragment>
+            // );
+
         }
 }
